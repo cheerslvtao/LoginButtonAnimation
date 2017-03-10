b536e9fd1d9507dd27bb9a628999e70610d9fa82
SFC-a <sfc-a@sfcdeMacBook-Pro.local>
2017-03-10 10:52:55 +0800

    Initial Commit

---
+++ LoginButtonAnimation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LoginButtonAnimation
+//
+//  Created by SFC-a on 2017/3/10.
+//  Copyright © 2017年 SFC-a. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
